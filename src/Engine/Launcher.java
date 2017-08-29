e8db731ea9a41ca78957f2cd3959fe586b324f84
Crumbletheundead <31432625+Crumbletheundead@users.noreply.github.com>
2017-08-29 15:04:46 +0800

    Update Launcher.java

--- src/Engine/Launcher.java
+++ src/Engine/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		
-		//this is to be used in 
+		//This needs to be used first to ensure that the GUI starts in the EDT Thread
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	            public void run() {
 	                new Engine(500, 600);
